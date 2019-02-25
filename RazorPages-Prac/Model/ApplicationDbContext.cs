81f7a7fbb80e3a77c32936c0080adb4b7a833b11
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-25 18:00:30 +0500

    ✨🗃👍 Add: Setting up DB connection. & AJAX Token

---
+++ RazorPages-Prac/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPages_Prac.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+
+    }
+}
